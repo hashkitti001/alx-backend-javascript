2f7557431e45c4838e6834605520c11e97f68f2e
hashkitti001 <gidemho@gmail.com>
2024-10-04 00:23:56 +0100

    add 0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a class room
+ */
+export default class ClassRoom {
+   /**
+    * Creates a new {@link ClassRoom}
+    * @param {Number} maxStudentSize - The max number
+    * of students in the class
+    */
+    constructor(maxStudentSize){
+        this. _maxStudentSize = maxStudentSize;
+    }
+}
