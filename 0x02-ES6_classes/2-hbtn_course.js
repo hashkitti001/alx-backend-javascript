ceb0d8b16c2c08349be802404013db990c63e5ea
hashkitti001 <gidemho@gmail.com>
2024-10-04 00:54:42 +0100

    fix : 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,72 +1,71 @@
 /**
- * Represents a Holberton Course
+ * Represents a Holberton Course.
  */
-class HolbertonCourse {
-  /**
-  * Creates a new @see {@link HolbertonCourse}.
-  *
-  * @param {String} name - The name of the course.
-  * @param {Number} length - How long the course is (in months).
-  * @param {String[]} students - The names of students in the course.
-  */
-  constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
-
-  /**
-     * Gets the name of the course
+export default class HolbertonCourse {
+    /**
+     * Creates a new @see {@link HolbertonCourse}.
+     *
+     * @param {String} name - The name of the course.
+     * @param {Number} length - How long the course is (in months).
+     * @param {String[]} students - The names of students in the course.
      */
-  get name() {
-    return this.name;
-  }
-
-  /**
-     * Sets the name of the course
-     * @param {value} value - The new value name should be set to
-     */
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
+    constructor(name, length, students) {
+      this.name = name;
+      this.length = length;
+      this.students = students;
     }
-    this._name = value;
-  }
-
-  /**
-     * Gets the length of the course
+  
+    /**
+     * Gets the name of this course.
      */
-  get length() {
-    return this._length;
-  }
-
-  /**
-    * Sets the length of the course
-    * @param {value} value - The new value length should be set to
-    */
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number');
+    get name() {
+      return this._name;
     }
-    this._length = length;
-  }
-
-  /**
-     * Gets the names of students in the course
+  
+    /**
+     * Sets the name of this course.
      */
-  get students() {
-    return this._students;
-  }
-
-  /**
-    * Sets the lists students of the course
-    * @param {value} value - The new value students should be set to
-    */
-  set students(value) {
-    if (!Array.isArray(value)) {
-      throw new TypeError('Students must be an array of strings');
+    set name(value) {
+      if (typeof value !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = value;
     }
-    this._students = value;
+  
+    /**
+     * Gets the length of this course (in months).
+     */
+    get length() {
+      return this._length;
+    }
+  
+    /**
+     * Sets the length of this course (in months).
+     */
+    set length(value) {
+      if (typeof value !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
+      this._length = value;
+    }
+  
+    /**
+     * Gets the names of students in this course.
+     */
+    get students() {
+      return this._students;
+    }
+  
+    /**
+     * Sets the names of students in this course.
+     */
+    set students(value) {
+      if (!(value instanceof Array)) {
+        throw new TypeError('Students must be an array of strings');
+      }
+      if (!value.every((student) => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
+      this._students = value;
+    }
   }
-}
-export default HolbertonCourse;
