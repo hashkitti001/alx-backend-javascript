7d8c23ab1807b124b3fb937c0ca223a427f1b48f
hashkitti001 <gidemho@gmail.com>
2024-10-07 20:19:33 +0100

    git add 0x03-data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+function getListStudents() {
+  const result = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return result;
+}
+export default getListStudents;
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,2 @@
+# 0x03-Data manipulation
+Tasks on JavaScript arrays, array methods and typed arrays
