85b4e5f18058065aacbbdfd8b9d6cb0680f11303
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-26 16:55:26 +0100

    add 0x05

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message to the STDOUT.
+ * @param {str} msg - String to be printed. 
+ */
+function displayMessage(msg) {
+    console.log(msg)
+}
+
+module.exports = displayMessage
